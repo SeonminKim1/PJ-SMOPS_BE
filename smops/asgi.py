5b3b8dc3b6b862c2146c2ba9f11122e471662247
SeonminKim1 <yubi5050@naver.com>
2022-06-29 09:18:30 +0900

    Init Baseline

---
+++ smops/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for smops project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smops.settings')
+
+application = get_asgi_application()
