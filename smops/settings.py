9d7540e47aa2bf09cb747fe1306cd611a5efbd23
SeonminKim1 <yubi5050@naver.com>
2022-07-05 16:22:02 +0900

    IP 수정

--- smops/settings.py
+++ smops/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = my_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.35.220.4']
+ALLOWED_HOSTS = ['3.34.3.15']
 
 # Application definition
 
